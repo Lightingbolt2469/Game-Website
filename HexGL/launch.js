53661b00d34ec235caa32d9a6400bdef04062d1d
Lightingbolt2469 <vihaancool23@gmail.com>
2026-04-03 17:54:09 -0400

    Update launch.js

--- HexGL/launch.js
+++ HexGL/launch.js
@@ -8,6 +8,10 @@
 
   init = function(controlType, quality, hud, godmode) {
     var hexGL, progressbar;
+    
+    // --- SAMSUNG/ANDROID FIX: Stop the browser from interfering ---
+    document.addEventListener('touchstart', function(e) { e.preventDefault(); }, {passive: false});
+    
     hexGL = new bkcore.hexgl.HexGL({
       document: document,
       width: window.innerWidth,
@@ -26,17 +30,13 @@
     progressbar = $('progressbar');
     return hexGL.load({
       onLoad: function() {
-        console.log('LOADED.');
         hexGL.init();
         $('step-3').style.display = 'none';
         $('step-4').style.display = 'block';
         return hexGL.start();
       },
-      onError: function(s) {
-        return console.error("Error loading " + s + ".");
-      },
+      onError: function(s) { return console.error("Error: " + s); },
       onProgress: function(p, t, n) {
-        console.log("LOADED " + t + " : " + n + " ( " + p.loaded + " / " + p.total + " ).");
         return progressbar.style.width = "" + (p.loaded / p.total * 100) + "%";
       }
     });
@@ -44,25 +44,24 @@
 
   u = bkcore.Utils.getURLParameter;
 
-  defaultControls = bkcore.Utils.isTouchDevice() ? 1 : 0;
+  // --- FORCE TOUCH FOR SAMSUNG GALAXY TABS ---
+  defaultControls = 1; // Since it's your tablet, let's just force Touch mode on by default.
 
-  s = [['controlType', ['KEYBOARD', 'TOUCH', 'LEAP MOTION CONTROLLER', 'GAMEPAD'], defaultControls, defaultControls, 'Controls: '], ['quality', ['LOW', 'MID', 'HIGH', 'VERY HIGH'], 3, 3, 'Quality: '], ['hud', ['OFF', 'ON'], 1, 1, 'HUD: '], ['godmode', ['OFF', 'ON'], 0, 1, 'Godmode: ']];
+  s = [
+    ['controlType', ['KEYBOARD', 'TOUCH', 'LEAP', 'GAMEPAD'], defaultControls, defaultControls, 'Controls: '],
+    ['quality', ['LOW', 'MID', 'HIGH', 'VERY HIGH'], 0, 0, 'Quality: '], // Default to LOW for smoother Android FPS
+    ['hud', ['OFF', 'ON'], 1, 1, 'HUD: '],
+    ['godmode', ['OFF', 'ON'], 0, 1, 'Godmode: ']
+  ];
 
   _fn = function(a) {
     var e, f, _ref;
     a[3] = (_ref = u(a[0])) != null ? _ref : a[2];
     e = $("s-" + a[0]);
-    (f = function() {
-      return e.innerHTML = a[4] + a[1][a[3]];
-    })();
-    return e.onclick = function() {
-      return f(a[3] = (a[3] + 1) % a[1].length);
-    };
+    (f = function() { return e.innerHTML = a[4] + a[1][a[3]]; })();
+    return e.onclick = function() { f(a[3] = (a[3] + 1) % a[1].length); };
   };
-  for (_i = 0, _len = s.length; _i < _len; _i++) {
-    a = s[_i];
-    _fn(a);
-  }
+  for (_i = 0, _len = s.length; _i < _len; _i++) { a = s[_i]; _fn(a); }
 
   $('step-2').onclick = function() {
     $('step-2').style.display = 'none';
@@ -70,47 +69,5 @@
     return init(s[0][3], s[1][3], s[2][3], s[3][3]);
   };
 
-  $('step-5').onclick = function() {
-    return window.location.reload();
-  };
-
-  $('s-credits').onclick = function() {
-    $('step-1').style.display = 'none';
-    return $('credits').style.display = 'block';
-  };
-
-  $('credits').onclick = function() {
-    $('step-1').style.display = 'block';
-    return $('credits').style.display = 'none';
-  };
-
-  hasWebGL = function() {
-    var canvas, gl;
-    gl = null;
-    canvas = document.createElement('canvas');
-    try {
-      gl = canvas.getContext("webgl");
-    } catch (_error) {}
-    if (gl == null) {
-      try {
-        gl = canvas.getContext("experimental-webgl");
-      } catch (_error) {}
-    }
-    return gl != null;
-  };
-
-  if (!hasWebGL()) {
-    getWebGL = $('start');
-    getWebGL.innerHTML = 'WebGL is not supported!';
-    getWebGL.onclick = function() {
-      return window.location.href = 'http://get.webgl.org/';
-    };
-  } else {
-    $('start').onclick = function() {
-      $('step-1').style.display = 'none';
-      $('step-2').style.display = 'block';
-      return $('step-2').style.backgroundImage = "url(css/help-" + s[0][3] + ".png)";
-    };
-  }
-
+  // ... (keep the rest of your file the same)
 }).call(this);
